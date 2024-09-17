6dc99556ba343acb36e39f2c7f515e397e44c8de
Prince <ashnand2000@gmail.com>
2024-09-18 00:33:44 +0530

    BANK:15. Swagger - Account DTO Annotation and Documentation

--- src/main/java/com/project/banking_app_spring/dto/AccountDto.java
+++ src/main/java/com/project/banking_app_spring/dto/AccountDto.java
@@ -1,12 +1,28 @@
 package com.project.banking_app_spring.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Schema(
+        description = "Account Dto Model Information"
+)
 public class AccountDto {
+
+    @Schema(
+            description = "User Account Id"
+    )
     private Long id;
+
+    @Schema(
+            description = "User Account Name"
+    )
     private String accountHolderName;
+
+    @Schema(
+            description = "User Account balance"
+    )
     private double balance;
 }
