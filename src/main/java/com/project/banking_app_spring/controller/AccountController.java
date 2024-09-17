4dbe209efe124c9a4b61d055830ba779ae4c410f
Prince <ashnand2000@gmail.com>
2024-09-18 00:31:26 +0530

    BANK:13. Swagger - Controller Annotation and Documentation

--- src/main/java/com/project/banking_app_spring/controller/AccountController.java
+++ src/main/java/com/project/banking_app_spring/controller/AccountController.java
@@ -2,6 +2,9 @@ package com.project.banking_app_spring.controller;
 
 import com.project.banking_app_spring.dto.AccountDto;
 import com.project.banking_app_spring.service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +16,23 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/accounts")
 @AllArgsConstructor
+@Tag(
+        name = "Banking App CRUD REST APIs for User Resource",
+        description = "CRUD REST APIs - Create User, Update User, Get User, Get All Users, Delete User"
+)
 public class AccountController {
 
     private AccountService accountService;
 
     // Add Account REST API
+    @Operation(
+            summary = "Create User and Amount REST API",
+            description = "Create User REST API is used to save user in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping("create")
     public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto){
 
@@ -26,6 +41,14 @@ public class AccountController {
     }
 
     // Get Account REST API
+    @Operation(
+            summary = "Get User and Amount By ID REST API",
+            description = "Get User By ID REST API is used to get a single user from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("getAccountById/{id}")
     public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id){
         AccountDto accountDto = accountService.getAccountById(id);
@@ -33,6 +56,14 @@ public class AccountController {
     }
 
     // Deposit REST API
+    @Operation(
+            summary = "Deposit Amount REST API",
+            description = "Deposit Amount REST API from the bank"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @PutMapping("/{id}/deposit")
     public ResponseEntity<AccountDto> deposit(@PathVariable Long id,
                                               @RequestBody Map<String, Double> request){
@@ -43,6 +74,14 @@ public class AccountController {
     }
 
     // Withdraw REST API
+    @Operation(
+            summary = "Withdraw Amount REST API",
+            description = "Withdraw Amount REST API from the bank"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @PutMapping("/{id}/withdraw")
     public ResponseEntity<AccountDto> withdraw(@PathVariable Long id,
                                                @RequestBody Map<String, Double> request){
@@ -53,6 +92,14 @@ public class AccountController {
     }
 
     // Get All Accounts REST API
+    @Operation(
+            summary = "Get All Users and Amount REST API",
+            description = "Get All Users and Amount REST API is used to get a all the users from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("allAccounts")
     public ResponseEntity<List<AccountDto>> getAllAccounts(){
         List<AccountDto> accounts = accountService.getAllAccounts();
@@ -60,6 +107,14 @@ public class AccountController {
     }
 
     // Delete Account REST API
+    @Operation(
+            summary = "Delete User REST API",
+            description = "Delete User REST API is used to delete a particular user from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @DeleteMapping("deleteAccount/{id}")
     public ResponseEntity<String> deleteAccount(@PathVariable Long id){
         accountService.deleteAccount(id);
